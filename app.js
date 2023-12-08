d90f5362da3bd234ffd4ac2b565bc6c76935172c
ocasergi@gmail.com <ocasergi@gmail.com>
2023-12-08 15:32:33 +0100

    removing paths

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.listen(4000, () => console.log('listening for requests on port 4000'));
 app.use(express.json());
 
 // Set up rate limiter: maximum of twenty requests per minute
-const RateLimit = require("express-rate-limit");
+const RateLimit = require('express-rate-limit');
 const limiter = RateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minute
 	max: 20,
@@ -32,14 +32,3 @@ app.post('/openai/text', generateText);
 app.post('/openai/summarize', summarizeText);
 app.post('/openai/keywords', generateKeywords);
 app.post('/openai/image', generateImage);
-
-
-const path = require('path');
-
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname + '/../frontend/dist/index.html'));
-})
